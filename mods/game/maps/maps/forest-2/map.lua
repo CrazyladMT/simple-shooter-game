e97de48d7dd71bac31ed6c2aa2bc42d86563f65a
a-bad-dev <244852891+a-bad-dev@users.noreply.github.com>
2025-12-11 23:43:50 -0400

    Add new map forest-2

---
+++ mods/game/maps/maps/forest-2/map.lua
@@ -0,0 +1,14 @@
+local map_data = {
+	name = "forest-2",
+	size_x = 189,
+	size_y = 71,
+	size_z = 102,
+	
+	barrier_level = 67, -- <- Y level of the barrier
+	
+	spawn_x = nil,
+	spawn_y = nil,
+	spawn_z = nil
+}
+
+return map_data
