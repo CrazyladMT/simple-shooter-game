4d5907f3f737f67a8f8271151af42bb3b0bc5b22
a-bad-dev <redacted>
2025-12-31 23:24:34 -0400

    Add structures to forest-3

--- mods/game/maps/maps/forest-3/map.lua
+++ mods/game/maps/maps/forest-3/map.lua
@@ -1,10 +1,10 @@
 local map_data = {
 	name = "forest-3",
 	size_x = 537,
-	size_y = 142,
+	size_y = 117,
 	size_z = 244,
 	
-	barrier_level = 138, -- <- Y level of the barrier
+	barrier_level = 113, -- <- Y level of the barrier
 	
 	spawn_x = nil,
 	spawn_y = nil,
